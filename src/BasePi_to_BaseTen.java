c46606bf90ed279a9dfb848a71f07d80eda4e268
Torjusba <torjus.bakkene@gmail.com>
2015-10-13 17:49:56 +0200

    Moved to new workspace

--- src/BasePi_to_BaseTen.java
+++ src/BasePi_to_BaseTen.java
@@ -2,37 +2,35 @@ import java.util.Scanner;
 
 public class BasePi_to_BaseTen {
 	public static void main(String[] args) {
-		
-		
-		while (true)
-		{
-//Initialize variables 
-		double num = 0;
-		System.out.println("Number: ");
-		Scanner in = new Scanner(System.in);
-		try {
-			num = in.nextDouble();
-		} finally {}
-		int a = 0;
-		int x;
-		double ans = 0;
-		
-//Find amount of digits
-		while (true) { 
-			if ((num / Math.pow(Math.PI, a+1)) >= 1) {
-				a++;
-			}else{
-				break;
-			}
-		}
 
-//Calculate digits
-		for(int i = a; i >=-10; i--) {
-			x = (int)(num/Math.pow(Math.PI, i));
-			num -= x * Math.pow(Math.PI, i);
-			ans += ((double)x*Math.pow(10, i));
+		while (true) {
+			// Initialize variables
+			double num = 0;
+			System.out.println("Number: ");
+			Scanner in = new Scanner(System.in);
+			try {
+				num = in.nextDouble();
+			} finally {}
+			int a = 0;
+			int x;
+			double answer = 0;
+
+			// Find amount of digits
+			while (true) {
+				if ((num / Math.pow(Math.PI, a + 1)) >= 1) {
+					a++;
+				} else {
+					break;
+				}
+			}
+
+			// Calculate digits
+			for (int i = a; i >= -10; i--) {
+				x = (int) (num / Math.pow(Math.PI, i));
+				num -= x * Math.pow(Math.PI, i);
+				answer += ((double) x * Math.pow(10, i));
+			}
+			System.out.println(answer);
 		}
-		System.out.println(ans);
-	}
 	}
 }
