a977c59a58c77074cf4006299c227ca16acbc7c4
Torjusba <torjus.bakkene@gmail.com>
2015-10-13 18:08:21 +0200

    Deleted unneccesary files

--- src/BasePi_to_BaseTen.java
+++ src/BasePi_to_BaseTen.java
@@ -2,35 +2,25 @@ import java.util.Scanner;
 
 public class BasePi_to_BaseTen {
 	public static void main(String[] args) {
-
 		while (true) {
-			// Initialize variables
-			double num = 0;
-			System.out.println("Number: ");
-			Scanner in = new Scanner(System.in);
-			try {
-				num = in.nextDouble();
-			} finally {}
-			int a = 0;
-			int x;
-			double answer = 0;
+//Initialize variables
+			int num = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Number to convert to base Pi: ");
+		num = in.nextInt();
+		double result = 0;
+		int pow = 0;
+		int digit = 0;
 
-			// Find amount of digits
-			while (true) {
-				if ((num / Math.pow(Math.PI, a + 1)) >= 1) {
-					a++;
-				} else {
-					break;
-				}
-			}
-
-			// Calculate digits
-			for (int i = a; i >= -10; i--) {
-				x = (int) (num / Math.pow(Math.PI, i));
-				num -= x * Math.pow(Math.PI, i);
-				answer += ((double) x * Math.pow(10, i));
-			}
-			System.out.println(answer);
+//Convert to Base Pi
+		while (num > 0) {
+			digit = num % 10;
+			result += (digit * Math.pow(Math.PI, pow++));
+			num = num/10;
+		}
+		System.out.println(result);
 		}
 	}
+	
+	
 }
--- src/BaseTen_to_BasePi.java
+++ src/BaseTen_to_BasePi.java
@@ -2,25 +2,35 @@ import java.util.Scanner;
 
 public class BaseTen_to_BasePi {
 	public static void main(String[] args) {
-		while (true) {
-//Initialize variables
-			int num = 0;
-		Scanner in = new Scanner(System.in);
-		System.out.println("Number to convert: ");
-		num = in.nextInt();
-		double result = 0;
-		int pow = 0;
-		int digit = 0;
 
-//Convert to Base Pi
-		while (num > 0) {
-			digit = num % 10;
-			result += (digit * Math.pow(Math.PI, pow++));
-			num = num/10;
-		}
-		System.out.println(result);
+		while (true) {
+			// Initialize variables
+			double num = 0;
+			System.out.println("Number to convert to base Ten: ");
+			Scanner in = new Scanner(System.in);
+			try {
+				num = in.nextDouble();
+			} finally {}
+			int a = 0;
+			int x;
+			double answer = 0;
+
+			// Find amount of digits
+			while (true) {
+				if ((num / Math.pow(Math.PI, a + 1)) >= 1) {
+					a++;
+				} else {
+					break;
+				}
+			}
+
+			// Calculate digits
+			for (int i = a; i >= -10; i--) {
+				x = (int) (num / Math.pow(Math.PI, i));
+				num -= x * Math.pow(Math.PI, i);
+				answer += ((double) x * Math.pow(10, i));
+			}
+			System.out.println(answer);
 		}
 	}
-	
-	
 }
